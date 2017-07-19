1338eb91617bd449b84f1cf967c52d70756eca88
杨锡钰 <yangxiyu@hotelgg.com>
2017-07-19 21:18:41 +0800

    初始化空项目

---
+++ Weather/Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by 杨锡钰 on 2017/7/19.
+//  Copyright © 2017年 杨锡钰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by 杨锡钰 on 2017/7/19.
+//  Copyright © 2017年 杨锡钰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
